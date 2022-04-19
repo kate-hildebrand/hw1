7bd014e2fbbdee3cd34f8d753d40de996f033b46
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-04-19 02:09:18 +0000

    created relational tables

--- kmdb.sql
+++ kmdb.sql
@@ -101,9 +101,37 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS actors;
 
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_name TEXT,
+    year_released INTEGER, 
+    MPAA_rating TEXT
+);
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER
+);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT,
+    actor_id INTEGER,
+    movie_id INTEGER    
+);
+
+CREATE TAbLE actors ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
